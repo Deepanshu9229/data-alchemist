b7ba140ad3cd7a85d7b091faf3a8050b80fd12d6
Deepa <deepanshupatel9229@gmail.com>
2025-06-28 13:21:07 +0530

    css problem solved

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,11 @@
-
-
-import type { Config } from 'tailwindcss'
-
-const config: Config = {
-  // plugins: ["@tailwindcss/postcss"],
+/** @type {import('tailwindcss').Config} */
+const config = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
+     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
@@ -16,10 +14,11 @@ const config: Config = {
           50: '#eff6ff',
           500: '#3b82f6',
           600: '#2563eb',
-        }
+        },
       },
     },
   },
-  // plugins: ["@tailwindcss/postcss"],
-}
-export default config
+  plugins: [],
+};
+
+export default config;
